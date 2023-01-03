f05bccffefd11fa7a4fcee8bdafb841eb153630a
Mathpal <deepak.mathpal@accenture.com>
2023-01-03 17:58:13 +0530

    format change added inside evaluation

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,47 @@
 var jp = require('jsonpath');
 
+ //192.168.1.36
 const Home = () => {
+   const sampleData = "";
+
     const handleFormatClick = () => {
         const outputArea = document.querySelector(".large-area--output");
         const formatted = JSON.stringify(JSON.parse(outputArea.value), null, 4);
         outputArea.value = formatted;
     }
 
+    const handleFormatClickForRequest = () => {
+        const inputArea = document.querySelector(".large-area--input");
+        const formatted = JSON.stringify(JSON.parse(inputArea.value), null, 4);
+        inputArea.value = formatted;
+    }
+
+
     const handleEvaluateClick = () => {
         const inputArea = document.querySelector(".large-area--input");
         const inputField = document.querySelector(".longInput");
         const outputArea = document.querySelector(".large-area--output");
-    
-        const formatted = JSON.parse(inputArea.value);
-        // console.log("json is :" + formatted);
-        const query = inputField.value;
-        // console.log("query is:" + query);
 
-        var evaluatedValue =  jp.query(formatted, query);
+        const formatted = JSON.parse(inputArea.value);
+        const query = inputField.value;
+
+        var evaluatedValue = jp.query(formatted, query);
         const result = JSON.stringify(evaluatedValue);
-        outputArea.value = result;
+        if (result.length === 2 && result.includes("[]")) {
+            outputArea.value = "No Response. Please check your query";
+        } else {
+            outputArea.value = result;
+            handleFormatClick();
+        }
     }
 
     return (
         <div class="container">
-            <textarea className="large-area large-area--input" placeholder="Enter your JSON here..."></textarea>
+            <textarea className="large-area large-area--input" placeholder={sampleData}></textarea>
             <textarea readOnly class="large-area large-area--output" placeholder="Your JSON will appear here..."></textarea>
             <input id="inputfield" className="longInput" placeholder="$.phoneNumbers[:1].type"></input>
             <div class="controls"> <button onClick={handleEvaluateClick} className="controls__button controls__button--evaluate">Evaluate Your Query</button></div>
-            <div class="controls"> <button onClick={handleFormatClick} className="controls__button controls__button--format">Format Your Response</button></div>
+            <div class="controls"> <button onClick={handleFormatClickForRequest} className="controls__button controls__button--format">Format Your Request</button></div>
         </div>
     );
 }
